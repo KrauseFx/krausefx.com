0aef7fcf91b07ae35ac3f9a1658597ce7cdab76a
Felix Krause <krausefx@gmail.com>
2022-08-18 01:20:49 +0200

    Include JS code

---
+++ assets/inappbrowser/app_js/tiktok.js
@@ -0,0 +1,55 @@
+/* --------------------
+DISCLAIMER: 
+
+The code below was generated through https://inappbrowser.com
+which basically overrides many of the standard JavaScript functions to get alerted
+whenever the host iOS app runs JavaScript commands. The code below is not complete.
+For example, having "[object HTMLStyleElement]" would mean an object, of the type
+HTMLStyleElement is being used. However, there are no further insights on those.
+
+Also, there might be more JavaScript code that is being run, through
+https://developer.apple.com/documentation/webkit/wkcontentworld, which can't be detected
+by this tool.
+
+The code below is for educational purposes only, and does not reflect a 100% accurate
+representation of the JavaScript code that is being run.
+
+This file was generated on 2022-08-17
+*/
+
+
+
+HTMLDocument.createElement('style')
+
+[object HTMLStyleElement].type = 'text/css'
+[object HTMLStyleElement].innerText = 'img { -webkit-user-select: none; -webkit-touch-callout: none; }'
+HTMLDocument.getElementsByTagName('head')
+[object HTMLCollection]['0']
+window.removeEventListener('error')
+window.removeEventListener('unhandledrejection')
+window.addEventListener('unload', function () { [native code] }
+window.addEventListener('unload', function () { [native code] }
+
+HTMLDocument.addEventListener('click', function (n){u=void 0,n&&e!==n&&r({event:e=n,name:t})})
+
+HTMLDocument.addEventListener('keypress', function (n){var t;try{t=n.target}catch(n){return}var r=t&&t.tagName;r&&("INPUT"===r||"TEXTAREA"===r||t.isContentEditable)&&(u||i("input",e)(n),clearTimeout(u),u=window.setTimeout(function(){u=void 0},o))})
+
+window.addEventListener('error', function (n){n=t(n),n=n&&i(n);n&&r&&r(n)}
+window.addEventListener('unhandledrejection', function (n){n=t(n),n=n&&i(n);n&&r&&r(n)}
+window.addEventListener('error', function (n){n=mt(e(),n,u||"");n&&t&&t(n)}, true
+window.addEventListener('keydown', function (){t&&t({name:"LCPMonitor",lcp:e}),i()}, true
+
+window.addEventListener('click', function (){t&&t({name:"LCPMonitor",lcp:e}),i()}, true
+
+window.addEventListener('unload', function (){o(),Yn.forEach(function(n){window.removeEventListener(n,u,!0)})}
+window.addEventListener('beforeunload', function (){o(),Yn.forEach(function(n){window.removeEventListener(n,u,!0)})}
+window.addEventListener('pagehide', function (){o(),Yn.forEach(function(n){window.removeEventListener(n,u,!0)})}
+HTMLDocument.addEventListener('visibilitychange', function (n){w(r)?r(n):"hidden"===document.visibilityState&&t(n)})
+window.addEventListener('unload', function (){o&&!b()&&(A(!0),r&&r())}
+window.addEventListener('beforeunload', function (){o&&!b()&&(A(!0),r&&r())}
+window.addEventListener('pagehide', function (){o&&!b()&&(A(!0),r&&r())}
+HTMLDocument.addEventListener('visibilitychange', function (n){w(r)?r(n):"hidden"===document.visibilityState&&t(n)})
+window.addEventListener('error', function (n){var t,r=n||o.event||{};try{t=r.target||r.srcElement||{}}catch(r){return}(w((n=t).getAttribute)?n.getAttribute("integrity"):n.integrity)&&(n=w((n=t).getAttribute)?n.getAttribute("src"):n.src||n.href||"",t=(null===(t=t.tagName)||void 0===t?void 0:t.toLowerCase())||"",n&&t&&n!==location.href&&e(n))}, true
+HTMLDocument.querySelector('head > title')
+
+HTMLDocument.elementFromPoint(28.666656, 80)
