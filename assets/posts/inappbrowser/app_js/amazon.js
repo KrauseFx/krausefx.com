d99db4856b5e984432dce55ef9ad71c1b96f55d8
Felix Krause <krausefx@gmail.com>
2022-08-18 19:29:19 +0200

    Add final screenshots & js scripts

---
+++ assets/posts/inappbrowser/app_js/amazon.js
@@ -0,0 +1,28 @@
+/* --------------------
+DISCLAIMER: 
+
+The code below was generated through https://inappbrowser.com
+which basically overrides many of the standard JavaScript functions to get alerted
+whenever the host iOS app runs JavaScript commands. The code below is not complete.
+For example, having "[object HTMLMetaElement]" would mean an object, of the type
+HTMLMetaElement is being used. However, there are no further insights on those.
+
+Also, there might be more JavaScript code that is being run, through
+https://developer.apple.com/documentation/webkit/wkcontentworld, which can't be detected
+by this tool.
+
+The code below is for educational purposes only, and does not reflect a 100% accurate
+representation of the JavaScript code that is being run.
+
+This file was generated on 2022-08-17
+*/
+
+
+HTMLDocument.createElement('meta')
+  [object HTMLMetaElement].setAttribute
+HTMLMetaElement.setAttribute('name', 'viewport')
+  [object HTMLMetaElement].setAttribute
+HIMLMetaElement.setAttribute('content', 'width=device-width')
+
+HTMLDocument.getElementsByTagName('head')
+[object HIMLCollection][0]
